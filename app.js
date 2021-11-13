bbbf9b2e74aeece85ddb289d1464d3b7d1f88935
Victor Henrique <victorhenriqu3@outlook.com>
2021-11-12 22:06:40 -0400

    Estrutura da pokedex coms os 151 pokemons

---
+++ app.js
@@ -0,0 +1,27 @@
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+const generatePokemonPromises = () => Array(151).fill().map((_, index) =>
+  fetch(getPokemonUrl(index + 1)).then((response) => response.json()))
+
+const generateHTML = (pokemons) => pokemons.reduce((accumulator, { name, id, types, sprites }) => {
+  const Eltypes = types.map((typeInfo) => typeInfo.type.name);
+
+  accumulator += `
+            <li class="card ${Eltypes[0]}">
+              <img class="card-image " alt="${name}" src=${sprites.front_default}></img>
+              <h2 class="card-title">#${id}. ${name}</h2>
+              <p class="card-subtitle">${Eltypes.join(" | ")}</p>
+            </li>`;
+  return accumulator;
+}, '');
+
+
+const insertPokemons = pokemons => {
+  console.log(pokemons)
+  const ul = document.querySelector('[data-js="pokedex"]');
+  ul.innerHTML = pokemons;
+};
+
+const pokemonPromises = generatePokemonPromises();
+
+Promise.all(pokemonPromises).then(generateHTML).then(insertPokemons);
